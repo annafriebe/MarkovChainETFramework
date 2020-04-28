28ee19dccee16feecc2b1df002775fb6437a1654
Anna Friebe <anna.friebe@mdh.se>
2020-04-28 11:51:19 +0200

    Cleanup

--- exportTimingData/exportTimingDataToCSVSimpleMarkov.py
+++ exportTimingData/exportTimingDataToCSVSimpleMarkov.py
@@ -10,73 +10,35 @@ import csv
 import switchDataForCPU
 
 
-def periodicAdjustedTimes(times, period, factor = 1):
-    periodicAdjustedTimes = np.zeros(len(times))
+def timesAsArray(times):
+    timesArray = np.zeros(len(times), dtype=int)
     for i in range(len(times)):
-        periodicAdjustedTimes[i] = ((times[i]* factor) % period) / factor
-    return periodicAdjustedTimes
-
-def getTwoProcessStates(previousProcessList, processName):
-    states = np.zeros(len(previousProcessList), dtype=int)
-    for i in range(len(previousProcessList)):
-        process = previousProcessList[i]
-        if process.startswith(processName) or process == '':
-            states[i] = 0
-        else:
-            states[i] = 1
-    return states
-
-def timesToClockCycles(times, conversionFactor):
-    nClockCycles = np.zeros(len(times), dtype=int)
-    for i in range(len(times)):
-        nClockCycles[i] = (int) (times[i] * conversionFactor + 0.5)
-    return nClockCycles
+        timesArray[i] = times[i]
+    return timesArray
+    
 
 
-
-def exportTimingDataToCSV(filename, outputBaseFileName, processName, period):
+def exportTimingDataToCSV(filename, outputBaseFileName, processName):
     switchWakeupData = getSwitchAndWakeupDataForCPU(filename, '[003]')
 
     releaseTimeDict, schedulingTimeDict, executionTimeDict, previousProcessList,\
     wakeupInLatencyProcessList, wakeupInExecutionProcessList = \
         getTimeDicts(switchWakeupData, processName)
-    factor = 1
-    allSchedulingTimes = np.zeros(0)
-    allReleaseTimes = np.zeros(0)
-    factor = 1
 
-    for item in releaseTimeDict:
-        releaseTimeDict[item] = periodicAdjustedTimes(releaseTimeDict[item], period, factor)
-        if item == 'all':
-            allReleaseTimes = releaseTimeDict[item]
-
-    for item in schedulingTimeDict:
-        schedulingTimeDict[item] = periodicAdjustedTimes(schedulingTimeDict[item], period, factor)
-        if item == 'all':
-            allSchedulingTimes = schedulingTimeDict[item]
-
-    schedulingReleaseDiff = allSchedulingTimes - allReleaseTimes
-
-    twoProcesses = getTwoProcessStates(previousProcessList, processName)
-
-    clockCycleConversionFactor = 1
-    executionTimes = timesToClockCycles(executionTimeDict['all'], clockCycleConversionFactor)
-    wakeUpLatencies = timesToClockCycles(schedulingReleaseDiff, clockCycleConversionFactor)
+    executionTimes = timesAsArray(executionTimeDict['all'])
 
     executionTimes = executionTimes[250:len(executionTimes)-1]
-    wakeUpLatencies = wakeUpLatencies[250:len(wakeUpLatencies)-1]
-    twoProcesses = twoProcesses[250:len(twoProcesses)-1]
     stateTimesFileName = outputBaseFileName + '.csv'
     with open(stateTimesFileName, 'w', newline='') as f:
         timeswriter = csv.writer(f, delimiter=',')
-        timeswriter.writerow(("executionTime", "wakeUpLatency", "interfered"))
+        timeswriter.writerow(("executionTime",))
         for j in range(len(executionTimes)):
-            timeswriter.writerow((executionTimes[j], wakeUpLatencies[j], twoProcesses[j]))
+            timeswriter.writerow((executionTimes[j],))
         
         
-exportTimingDataToCSV('../simpleMarkov/reports/simpleMarkovTrainReport', '../modelIdentificationValidation/input/simpleMarkovTimesTrain', 'simpleMarkov', 5000000)
+exportTimingDataToCSV('../simpleMarkov/reports/simpleMarkovTrainReport', '../modelIdentificationValidation/input/simpleMarkovTimesTrain', 'simpleMarkov')
 for i in range(20):
     inputFileName = '../simpleMarkov/reports/simpleMarkovTestReport' + str(i+1)
     outputFileName = '../modelIdentificationValidation/input/simpleMarkovTimesTest' + str(i+1)
-    exportTimingDataToCSV(inputFileName, outputFileName, 'simpleMarkov', 5000000)
+    exportTimingDataToCSV(inputFileName, outputFileName, 'simpleMarkov')
     
--- exportTimingData/exportTimingDataToCSVVideoDecompression.py
+++ exportTimingData/exportTimingDataToCSVVideoDecompression.py
@@ -10,98 +10,59 @@ import csv
 import switchDataForCPU
 
 
-def periodicAdjustedTimes(times, period, factor = 1):
-    periodicAdjustedTimes = np.zeros(len(times))
+def timesAsArray(times):
+    timesArray = np.zeros(len(times), dtype=int)
     for i in range(len(times)):
-        periodicAdjustedTimes[i] = ((times[i]* factor) % period) / factor
-    return periodicAdjustedTimes
-
-def getTwoProcessStates(previousProcessList, processName):
-    states = np.zeros(len(previousProcessList), dtype=int)
-    for i in range(len(previousProcessList)):
-        process = previousProcessList[i]
-        if process.startswith(processName) or process == '':
-            states[i] = 0
-        else:
-            states[i] = 1
-    return states
-
-def timesToClockCycles(times, conversionFactor):
-    nClockCycles = np.zeros(len(times), dtype=int)
-    for i in range(len(times)):
-        nClockCycles[i] = (int) (times[i] * conversionFactor + 0.5)
-    return nClockCycles
+        timesArray[i] = times[i]
+    return timesArray
 
 
-
-def exportTimingDataToCSV(filename, outputBaseFileName, processName, period):
+def exportTimingDataToCSV(filename, outputBaseFileName, processName):
     switchWakeupData = getSwitchAndWakeupDataForCPU(filename, '[003]')
 
     releaseTimeDict, schedulingTimeDict, executionTimeDict, previousProcessList,\
     wakeupInLatencyProcessList, wakeupInExecutionProcessList = \
         getTimeDicts(switchWakeupData, processName)
-    factor = 1
-    allSchedulingTimes = np.zeros(0)
-    allReleaseTimes = np.zeros(0)
-    factor = 1
 
-    for item in releaseTimeDict:
-        releaseTimeDict[item] = periodicAdjustedTimes(releaseTimeDict[item], period, factor)
-        if item == 'all':
-            allReleaseTimes = releaseTimeDict[item]
-
-    for item in schedulingTimeDict:
-        schedulingTimeDict[item] = periodicAdjustedTimes(schedulingTimeDict[item], period, factor)
-        if item == 'all':
-            allSchedulingTimes = schedulingTimeDict[item]
-
-    schedulingReleaseDiff = allSchedulingTimes - allReleaseTimes
-
-    twoProcesses = getTwoProcessStates(previousProcessList, processName)
-
-    clockCycleConversionFactor = 1
-    executionTimes = timesToClockCycles(executionTimeDict['all'], clockCycleConversionFactor)
-    wakeUpLatencies = timesToClockCycles(schedulingReleaseDiff, clockCycleConversionFactor)
-
+    executionTimes = timesAsArray(executionTimeDict['all'])
     executionTimes = executionTimes[250:len(executionTimes)-50]
-    wakeUpLatencies = wakeUpLatencies[250:len(wakeUpLatencies)-50]
-    twoProcesses = twoProcesses[250:len(twoProcesses)-50]
+
     stateTimesFileName = outputBaseFileName + '.csv'
     with open(stateTimesFileName, 'w', newline='') as f:
         timeswriter = csv.writer(f, delimiter=',')
-        timeswriter.writerow(("executionTime", "wakeUpLatency", "interfered"))
+        timeswriter.writerow(("executionTime",))
         for j in range(len(executionTimes)):
-            timeswriter.writerow((executionTimes[j], wakeUpLatencies[j], twoProcesses[j]))
+            timeswriter.writerow((executionTimes[j],))
     stateTimesFileName = outputBaseFileName + 'MS1' + '.csv'
     with open(stateTimesFileName, 'w', newline='') as f:
         timeswriter = csv.writer(f, delimiter=',')
-        timeswriter.writerow(("executionTime", "wakeUpLatency", "interfered"))
+        timeswriter.writerow(("executionTime",))
         for j in range(len(executionTimes)):
             if (executionTimes[j] < 150000):
-                    timeswriter.writerow((executionTimes[j], wakeUpLatencies[j], twoProcesses[j]))
+                    timeswriter.writerow((executionTimes[j],))
     stateTimesFileName = outputBaseFileName + 'MS2' + '.csv'
     with open(stateTimesFileName, 'w', newline='') as f:
         timeswriter = csv.writer(f, delimiter=',')
-        timeswriter.writerow(("executionTime", "wakeUpLatency", "interfered"))
+        timeswriter.writerow(("executionTime",))
         for j in range(len(executionTimes)):
             if ((executionTimes[j] >= 150000) and (executionTimes[j] < 1000000)):
-                    timeswriter.writerow((executionTimes[j], wakeUpLatencies[j], twoProcesses[j]))
+                    timeswriter.writerow((executionTimes[j],))
         
     stateTimesFileName = outputBaseFileName + 'MS3' + '.csv'
     with open(stateTimesFileName, 'w', newline='') as f:
         timeswriter = csv.writer(f, delimiter=',')
-        timeswriter.writerow(("executionTime", "wakeUpLatency", "interfered"))
+        timeswriter.writerow(("executionTime", ))
         for j in range(len(executionTimes)):
             if ((executionTimes[j] >= 1000000) and (executionTimes[j] < 22500000) ):
-                timeswriter.writerow((executionTimes[j], wakeUpLatencies[j], twoProcesses[j]))
+                timeswriter.writerow((executionTimes[j],))
         
     stateTimesFileName = outputBaseFileName + 'MS4' + '.csv'
     with open(stateTimesFileName, 'w', newline='') as f:
         timeswriter = csv.writer(f, delimiter=',')
-        timeswriter.writerow(("executionTime", "wakeUpLatency", "interfered"))
+        timeswriter.writerow(("executionTime",))
         for j in range(len(executionTimes)):
             if ((executionTimes[j] >= 22500000) ):
-                timeswriter.writerow((executionTimes[j], wakeUpLatencies[j], twoProcesses[j]))
+                timeswriter.writerow((executionTimes[j],))
         
         
-exportTimingDataToCSV('../videoDecompression/traceVideoTestReport', '../modelIdentificationValidation/input/videoStateTimesTest', 'ffmpeg', 10000000)
+exportTimingDataToCSV('../videoDecompression/traceVideoTestReport', '../modelIdentificationValidation/input/videoStateTimesTest', 'ffmpeg')
